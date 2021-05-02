0f1e20d27e1a88598069fdc2655990a9fa3a13fc
Adam Czepiel <adam.czepiel@sd-technologies.de>
2021-05-02 04:23:26 +0200

    Unit Tests added

---
+++ NUnitTesting/UnitTest1.cs
@@ -0,0 +1,23 @@
+using _02AzureDevopsAspNetWebAppMVCWithDatabase.Controllers;
+using _02AzureDevopsAspNetWebAppMVCWithDatabase.Data;
+using NUnit.Framework;
+
+namespace NUnitTesting
+{
+    public class Tests
+    {
+       
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
